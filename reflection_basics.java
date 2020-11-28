e2b728c6a3b3e9fea8d4b7007088f87a03aba2d7
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-11-28 14:01:49 +0100

    adding 10 most common methods in the parent package Class class

--- reflection_basics.java
+++ reflection_basics.java
@@ -1,3 +1,13 @@
 // Overview methods of Class class in Java (in this case, they are all public)
 
 public String getName() {}
+public static Class forName (String className) throws ClassNotFoundException {}
+public Object newInstance() throws InstantiationException, IllegalAccessException {}
+public boolean isInterface() {}
+public boolean isArray() {}
+public boolean isPrimitive() {}
+public Class getSuperclass() {}
+public Field[] getDeclaredFields() throws SecurityException {}
+public Method[] getDeclaredMethods() throws SecurityException {}
+public Constructor[] getDeclaredConstructors() throws SecurityException {}
+public Method getDeclaredMethod (String name, Class[] parameterTypes) throws NoSuchMethodException, SecurityException  {}

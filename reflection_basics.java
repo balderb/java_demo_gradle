4ec1e5b48cc74a6fef10b7f6b81eeea20ed167c2
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-11-28 14:04:01 +0100

    adding comments to all methods from the parent Class class

--- reflection_basics.java
+++ reflection_basics.java
@@ -1,13 +1,35 @@
 // Overview methods of Class class in Java (in this case, they are all public)
 
 public String getName() {}
+// ⇒ Returns the class name.
+
 public static Class forName (String className) throws ClassNotFoundException {}
+// ⇒ Loads the class and returns the reference of Class class.
+
+
 public Object newInstance() throws InstantiationException, IllegalAccessException {}
+// ⇒ Creates new instance.
+
 public boolean isInterface() {}
+// ⇒ Checks if it is an interface.
+
 public boolean isArray() {}
+// ⇒ Checks if it is an array.
+
 public boolean isPrimitive() {}
+// ⇒ Checks if it is a primitive.
+
 public Class getSuperclass() {}
+// ⇒ Returns the superclass class reference.
+
 public Field[] getDeclaredFields() throws SecurityException {}
+// 
+
 public Method[] getDeclaredMethods() throws SecurityException {}
+// 
+
 public Constructor[] getDeclaredConstructors() throws SecurityException {}
+// 
+
 public Method getDeclaredMethod (String name, Class[] parameterTypes) throws NoSuchMethodException, SecurityException  {}
+// 
